6838775177bbf3d28d7f97f8c4194ddfb7a511ab
Siddhesh Tripathi <siddheshtripathi236@gmail.com>
2026-03-06 10:46:32 +0530

    data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -16,12 +16,12 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Enter the data ingestion method or component")
         try:
-            df = pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv('notebook/data/stud.csv')
             logging.info('Read the dataset as dataframe')
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok= True)
             df.to_csv(self.ingestion_config.raw_data_path,index = False,header= True)
             logging.info("Train test split initiated")
-            train_set , test_set = train_test_split(de,test_size=0.2,random_state= 42)
+            train_set , test_set = train_test_split(df,test_size=0.2,random_state= 42)
             train_set.to_csv(self.ingestion_config.train_data_path,index = False,header= True)
             test_set.to_csv(self.ingestion_config.test_data_path,index = False,header= True)
             logging.info("Ingestion of data is completed")
